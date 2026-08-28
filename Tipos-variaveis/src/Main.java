b81dea6cf47d49a693d475ff64d62e4e87d39e6b
Anthonny de Lima Lucena <anthonnylima17@gmail.com>
2026-08-28 09:52:14 -0300

    testes com os tipos primitivos java

---
+++ Tipos-variaveis/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double salarioMinmo = 2500;
+
+        short numeroPequeno=1;
+        int numeroPeqeuno2=numeroPequeno;
+
+        System.out.println(numeroPeqeuno2);
+        System.out.println(numeroPequeno);
+
+        final double PI=3.14;
+
+    }
+}
