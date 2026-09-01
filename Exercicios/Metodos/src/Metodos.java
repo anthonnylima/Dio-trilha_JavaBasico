e2e6bc30b597054de9ee54217ae53740a123e476
Anthonny de Lima Lucena <anthonnylima17@gmail.com>
2026-09-01 10:02:45 -0300

    trabalhando com metodos

---
+++ Exercicios/Metodos/src/Metodos.java
@@ -0,0 +1,11 @@
+public class Metodos{
+    public int somar(int n1,int n2){
+        int soma = n1+n2;
+        return soma;
+    }
+
+    private int somaPrivada(int n1, int n2){
+        int soma=n1+n2;
+        return soma;
+    }
+}
