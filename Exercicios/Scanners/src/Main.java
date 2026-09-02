712693c8725a16b70439d19dd36a5a99faa5a624
Anthonny de Lima Lucena <anthonnylima17@gmail.com>
2026-09-02 16:09:48 -0300

    atualizando

--- Exercicios/Scanners/src/Main.java
+++ Exercicios/Scanners/src/Main.java
@@ -1,7 +1,8 @@
+import java.util.Locale;
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome:");
         String nome = scanner.next();
@@ -9,7 +10,11 @@ public class Main{
         System.out.println("Digite sua idade:");
         int idade = scanner.nextInt();
 
+        System.out.println("Digite sua altura:");
+        double altura = scanner.nextDouble();
+
         System.out.println("Seu nome é:"+nome);
         System.out.println("Sua idade é:"+ idade);
+        System.out.println("Sua altura é:"+ altura+" cm");
     }
 }
