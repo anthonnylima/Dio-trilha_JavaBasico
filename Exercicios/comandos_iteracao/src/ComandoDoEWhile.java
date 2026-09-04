5421df9276cebf6bfd303e2e5db2aef0aa450fcc
Anthonny de Lima Lucena <anthonnylima17@gmail.com>
2026-09-04 12:06:23 -0300

    comandos de iteraçao

---
+++ Exercicios/comandos_iteracao/src/ComandoDoEWhile.java
@@ -0,0 +1,8 @@
+public class ComandoDoEWhile {
+    public static void main(String[] args) {
+        boolean executou=true;
+        do {
+            System.out.println("executa pelo menos uma vez!");
+        }while (!executou);
+    }
+}
---
+++ Exercicios/comandos_iteracao/src/ComandoFor.java
@@ -0,0 +1,11 @@
+public class ComandoFor {
+    public static void main(String[] args) {
+        int contador=0;
+
+        for (int x=0; x<10; x++){
+            contador++;
+        }
+
+        System.out.println(contador);
+    }
+}
