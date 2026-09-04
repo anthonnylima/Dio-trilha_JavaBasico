5d69e07257321c8af5574b87729925fc5bcbc850
Anthonny de Lima Lucena <anthonnylima17@gmail.com>
2026-09-04 14:44:55 -0300

    Comandos de iteração

---
+++ Exercicios/comandos_iteracao/src/ForArray.java
@@ -0,0 +1,9 @@
+public class ForArray {
+    public static void main(String[] args) {
+        String[] array={"Anthonny", "Nyara", "Andrielly", "Andressa"};
+
+        for (String a: array){
+            System.out.println(a);
+        }
+    }
+}
---
+++ Exercicios/comandos_iteracao/src/SwtichCase.java
@@ -0,0 +1,14 @@
+public class SwtichCase {
+    public static void main(String[] args) {
+        int opcao=0;
+
+        switch (opcao){
+            case 1:{
+                System.out.println("Primeita opcção");
+                break;
+            }
+            default:
+                System.out.println("opeção que roda se o valor naõ corresponder a nenhum case");
+        }
+    }
+}
