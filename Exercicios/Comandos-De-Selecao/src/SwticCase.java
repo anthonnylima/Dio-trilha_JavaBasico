db25b12afe0aa46449d4cc0bba4361a311417e3e
Anthonny de Lima Lucena <anthonnylima17@gmail.com>
2026-09-04 11:24:00 -0300

    switch-case

---
+++ Exercicios/Comandos-De-Selecao/src/SwticCase.java
@@ -0,0 +1,19 @@
+public class SwticCase {
+    public static void main(String[] args) {
+        String sigla="t";
+
+        switch (sigla){
+            case "t":{
+                System.out.println("5 Gb de youtube");
+            }
+
+            case "g":{
+                System.out.println("INSTRA e WPP ilimitados");
+            }
+
+            case "c":{
+                System.out.println("ligações ilimitadas");
+            }
+        }
+    }
+}
